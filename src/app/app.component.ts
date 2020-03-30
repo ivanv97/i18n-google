8115ec01493034fbf1e0fc682380904f66beef81
Ivan Velkushanov <ivanvelkushanov@deltasource.eu>
2020-03-30 17:24:42 +0300

    Redeploy #4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-root",
@@ -10,9 +11,15 @@ export class AppComponent {
   title = "i18n-google";
 
   constructor(
-    private translate: TranslateService
+    private translate: TranslateService,
+    private router: Router
   ) {
     this.translate.addLangs(["nl", "fr", "en"]);
     this.translate.setDefaultLang("nl");
+    let path = localStorage.getItem("path");
+    if (path) {
+      localStorage.removeItem("path");
+      this.router.navigate([path]);
+    }
   }
 }
