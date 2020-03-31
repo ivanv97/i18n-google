7e9f39c930234e1be69f7077b5ea2c7d11dc04e4
Ivan Velkushanov <ivanvelkushanov@deltasource.eu>
2020-03-31 12:01:06 +0300

    Go back to using ngx-translate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { TranslateSampleComponent } from "./translate-sample/translate-sample.component";
 import { AppComponent } from "./app.component";
-import { EnglishComponent } from "./english/english.component";
-import { DutchComponent } from "./dutch/dutch.component";
-import { FrenchComponent } from "./french/french.component";
 
 const routes: Routes = [
-  { path: "translate/en", component: EnglishComponent },
-  { path: "translate/nl", component: DutchComponent },
-  { path: "translate/fr", component: FrenchComponent },
+  { path: "translate/:lang", component: TranslateSampleComponent },
   { path: "", component: AppComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TranslateSampleComponent } from './translate-sample/translate-sample.component';
-import { EnglishComponent } from './english/english.component';
-import { FrenchComponent } from './french/french.component';
-import { DutchComponent } from './dutch/dutch.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TranslateSampleComponent } from "./translate-sample/translate-sample.component";
 
 @NgModule({
-  declarations: [AppComponent, TranslateSampleComponent, EnglishComponent, FrenchComponent, DutchComponent],
+  declarations: [AppComponent, TranslateSampleComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
