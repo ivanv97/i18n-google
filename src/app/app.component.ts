0be2893c61e4bfe74666ee0d4416292ac9a51f34
Ivan Velkushanov <ivanvelkushanov@deltasource.eu>
2020-04-16 14:19:01 +0300

    Add gtag for google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,30 @@
 import { Component } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from "@angular/router";
+declare let gtag;
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
   title = "i18n-google";
 
-  constructor(
-    private translate: TranslateService,
-    private router: Router
-  ) {
-    this.translate.addLangs(["nl", "fr", "en"]);
-    this.translate.setDefaultLang("nl");
+  constructor(private _translate: TranslateService, private _router: Router) {
+    this._translate.addLangs(["nl", "fr", "en"]);
+    this._translate.setDefaultLang("nl");
     let path = localStorage.getItem("path");
     if (path) {
       localStorage.removeItem("path");
-      this.router.navigate([path]);
+      this._router.navigate([path]);
     }
+    this._router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag("config", "UA-163765776-1", {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    });
   }
 }
